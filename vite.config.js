f46c2c13c7397bd4e1ed29377e02d6434f2a8f8b
mr-d-nobody <mr-d-nobody@users.noreply.github.com>
2026-05-03 15:32:44 +0530

    Switch to Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/MarksCalc/",
   plugins: [react(), tailwindcss()],
 })
